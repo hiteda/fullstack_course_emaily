9ba85c7055e744cfea36053fb511159d71ec6ff0
David K Hite <david.k.hite@gmail.com>
2021-02-24 21:45:14 -0800

    Add react frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user'); // Executes contents of user.js
 require('./services/passport');
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 // Enable cookies and have passport handle them
 app.use(
     cookieSession({
@@ -21,6 +23,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Make sure express serves production assets (i.e.
+    // main.js, main.css)
+    app.use(express.static('client/build'));
+
+    // Make express serve up index.html if it doesn't
+    // recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // If process.env.PORT (environment variable) isn't set, use 5000
 const PORT = process.env.PORT || 5000;
